672551d31086fe1392283a615fce1f5e3f840baf
xprim <me@asobirov.uz>
2022-06-12 20:20:03 +0500

    Fix auth (finally i hope)

--- src/common/lib/prisma.ts
+++ src/common/lib/prisma.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export { prisma };
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,21 @@
+import type { NextApiHandler } from "next";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+import { prisma } from "@lib/prisma";
 
-export default NextAuth({
-    adapter: PrismaAdapter(prisma),
-    secret: process.env.NEXT_AUTH_SECRET,
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        })
-    ]
-})
+        }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXT_AUTH_SECRET,
+};
