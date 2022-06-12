3aa875f0dac0c66f8d5dcedd4ac36545d2562bef
xprim <me@asobirov.uz>
2022-06-12 20:25:35 +0500

    Add layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,20 @@ import type { AppProps } from 'next/app'
 
 import { SessionProvider } from "next-auth/react";
 import PageLayout from '@components/Layout/PageLayout';
+import { AppPropsWithLayout } from 'src/common/types';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || (
+    (page) => (
+      <PageLayout>
+        {page}
+      </PageLayout>
+    )
+  );
   return (
     <SessionProvider session={pageProps.session}>
       <PageLayout>
-        <Component {...pageProps} />
+        {getLayout(<Component {...pageProps} />)}
       </PageLayout>
     </SessionProvider>
 
