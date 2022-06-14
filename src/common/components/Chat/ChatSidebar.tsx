91b3f2b83feababeceb0a896651994667ca23a08
xprim <me@asobirov.uz>
2022-06-14 22:03:52 +0500

    Fix styles

--- src/common/components/Chat/ChatSidebar.tsx
+++ src/common/components/Chat/ChatSidebar.tsx
@@ -5,7 +5,7 @@ type ChatSideBarProps = {
 
 export const ChatSidebar: React.FC<ChatSideBarProps> = () => {
     return (
-        <div className='max-w-sm border-r border-r-neutral-500'>
+        <div className='w-full max-w-sm border-r border-r-neutral-500'>
             <div className="flex flex-col sticky top-0 h-screen">
                 <ChatPreview time={new Date()} title="Kawey Nest" />
                 <ChatPreview time={new Date()} title="Kawey Nest" />
