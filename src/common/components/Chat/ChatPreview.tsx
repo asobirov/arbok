00d292d2ac1c3a8c70c18d248942758973f8f52d
xprim <me@asobirov.uz>
2022-06-20 00:43:24 +0500

    Minor UI changes

--- src/common/components/Chat/ChatPreview.tsx
+++ src/common/components/Chat/ChatPreview.tsx
@@ -1,15 +1,24 @@
+import { DOMAttributes, memo } from "react";
+import clsx from "clsx";
+
 import { Avatar, AvatarProps } from "@components/User/Avatar"
-import { useSession } from "next-auth/react"
-import { useRouter } from "next/router";
-import { useCallback } from "react";
 
-type ChatPreviewProps = Pick<AvatarProps, "src"> & {
-    title: string;
-    time: Date;
-    lastMessage?: string;
-}
+type ChatPreviewProps =
+    Pick<DOMAttributes<HTMLDivElement>, "onClick">
+    & Pick<AvatarProps, "src">
+    & {
+        title: string;
+        time: Date;
+        lastMessage?: string;
 
-export const ChatPreview: React.FC<ChatPreviewProps> = ({ title, time, src, lastMessage }) => {
+        isActive?: boolean;
+    }
+
+const ChatPreviewComponent: React.FC<ChatPreviewProps> = ({
+    title, time, src, lastMessage,
+    isActive,
+    onClick,
+}) => {
     const getTimeString = (time?: Date): string => {
         if (!time) {
             return "";
@@ -29,8 +38,15 @@ export const ChatPreview: React.FC<ChatPreviewProps> = ({ title, time, src, last
     }
 
     return (
-        <div className="flex w-full">
-            <div className="flex flex-row flex-1  px-5 py-3 w-full">
+        <div
+            className={clsx("group flex py-[0.125rem] px-2 w-full transition-all duration-100 ease-in",
+                "hover:cursor-pointer",
+                {
+                    "bg-black": isActive,
+                })}
+            onClick={onClick}
+        >
+            <div className={`flex flex-row flex-1 w-full px-4 py-3 rounded-3xl group-hover:bg-blackAlpha-700`}>
                 <div>
                     <Avatar
                         alt={title}
@@ -39,13 +55,13 @@ export const ChatPreview: React.FC<ChatPreviewProps> = ({ title, time, src, last
                         src={src}
                     />
                 </div>
-                <div className="overflow-hidden flex flex-grow flex-col justify-center ml-2">
-                    <div className="flex flex-row justify-between">
-                        <span className="text-base font-normal text-white">{title}</span>
-                        <span className="text-xs text-neutral-400">{getTimeString(time)}</span>
+                <div className="flex overflow-hidden flex-col grow justify-center ml-2">
+                    <div className="flex flex-row justify-between items-center">
+                        <span className="text-base font-medium text-neutral-100">{title}</span>
+                        <span className="text-xs text-gray-400">{getTimeString(time)}</span>
                     </div>
                     <div className="flex justify-center items-center">
-                        <span className="truncate flex-grow text-xs w-full text-neutral-300">
+                        <span className="grow w-full text-sm font-light text-neutral-200 truncate">
                             {lastMessage ?? "No messages"}
                         </span>
                     </div>
@@ -54,3 +70,5 @@ export const ChatPreview: React.FC<ChatPreviewProps> = ({ title, time, src, last
         </div>
     )
 }
+
+export const ChatPreview = memo(ChatPreviewComponent);
--- src/common/components/Chat/ChatSidebar.tsx
+++ src/common/components/Chat/ChatSidebar.tsx
@@ -1,16 +1,29 @@
+import { useCallback, useState } from "react";
 import { ChatPreview } from "./ChatPreview";
 
 type ChatSideBarProps = {
 };
 
 export const ChatSidebar: React.FC<ChatSideBarProps> = () => {
+    const [activeChat, setActiveChat] = useState<string | null>(null);
+    const handleChatPreviewClick = useCallback((id: string) => {
+        setActiveChat(id);
+    }, [])
+
     return (
-        <div className='w-full max-w-sm border-r border-r-neutral-500'>
-            <div className="flex flex-col sticky top-0 h-screen">
-                <ChatPreview time={new Date()} title="Kawey Nest" />
-                <ChatPreview time={new Date()} title="Kawey Nest" />
-                <ChatPreview time={new Date()} title="Kawey Nest" />
-                <ChatPreview time={new Date()} title="Kawey Nest" />
+        <div className='w-full max-w-sm border-r border-r-neutral-800'>
+            <div className="flex overflow-y-scroll sticky top-0 flex-col h-screen">
+                {Array.from({ length: 20 }).map((_, key) => (
+                    <>
+                        <ChatPreview
+                            key={key}
+                            time={new Date()}
+                            title={`Kawey Nest ${key}`}
+                            isActive={activeChat === `${key}`}
+                            onClick={() => handleChatPreviewClick(`${key}`)}
+                        />
+                    </>
+                ))}
             </div>
         </div>
     )
